6a4ea8847912b0e32c2a8e31663fead7e7c40e11
system-conf <suleyman.talha.duman0@gmail.com>
2023-10-01 13:54:16 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,125 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SectionList,
+  SafeAreaView,
+  Image,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+
+
+
+const ListItem = ({ item }) => {
+  return (
+    <View style={styles.item}>
+      <TouchableOpacity onPress={item.uri}>
+        <Image
+          source={{
+            uri: item.uri,
+          }}
+          style={styles.itemPhoto}
+          resizeMode="cover"
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default () => {
+  
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <SectionList
+          contentContainerStyle={{ paddingHorizontal: 10 }}
+          stickySectionHeadersEnabled={false}
+          sections={SECTIONS}
+          renderSectionHeader={({ section }) => (
+            <>
+              <Text style={styles.sectionHeader}>{section.title}</Text>
+              {section.horizontal ? (
+                <FlatList
+                  horizontal
+                  data={section.data}
+                  renderItem={({ item }) => <ListItem item={item} />}
+                  showsHorizontalScrollIndicator={false}
+                />
+              ) : null}
+            </>
+          )}
+          renderItem={({ item, section }) => {
+            if (section.horizontal) {
+              return null;
+            }
+            return <ListItem item={item} />;
+          }}
+        />
+      </SafeAreaView>
+    </View>
+  );
+};
+
+const SECTIONS = [
+  {
+    title: "system.conf horizontal scroll",
+    horizontal: true,
+    data: [
+      {
+        key: "1",
+        text: "Item text 1",
+        uri: "https://picsum.photos/id/1/200",
+      },
+      {
+        key: "2",
+        text: "Item text 2",
+        uri: "https://picsum.photos/id/10/200",
+      },
+
+      {
+        key: "3",
+        text: "Item text 3",
+        uri: "https://picsum.photos/id/1002/200",
+      },
+      {
+        key: "4",
+        text: "Item text 4",
+        uri: "https://picsum.photos/id/1006/200",
+      },
+      {
+        key: "5",
+        text: "Item text 5",
+        uri: "https://picsum.photos/id/1008/200",
+      },
+    ],
+  },
+];
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121212",
+  },
+  sectionHeader: {
+    fontWeight: "800",
+    fontSize: 18,
+    color: "#f4f4f4",
+    marginTop: 200,
+    marginBottom: 5,
+  },
+  item: {
+    margin: 10,
+  },
+  itemPhoto: {
+    width: 100,
+    height: 100,
+  },
+  itemText: {
+    color: "rgba(255, 255, 255, 0.5)",
+    marginTop: 5,
+  },
+});
